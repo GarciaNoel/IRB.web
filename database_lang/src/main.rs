0b26410abcc8b1d67c9c3fb3dc4ba2c492e3df79
GarciaNoel <n6garcia@ucsd.edu>
2025-09-26 12:33:31 -0700

    cargo datapack

--- database_lang/src/main.rs
+++ database_lang/src/main.rs
@@ -1,5 +1,8 @@
 // Author: Noel Garcia
 
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 // fn main got lost in a sea or pool of nnothing..
 fn main() {
     main_ad();
@@ -10,6 +13,10 @@ struct cargo {
     rec: i32,
 }
 
+struct data_pack {
+    datum: Vec<u8>,
+}
+
 impl cargo {
     fn send(&self) -> u32 {
         let x = self.tel as i32;
@@ -17,22 +24,73 @@ impl cargo {
         let x1 = x0 as u32;
         return x1;
     }
+
+    pub fn pipeclean(&self) {
+        let dat = data_pack {
+            datum: vec![1,2,3,4,5,6,7,8,9,0],
+        };
+                
+        let mut rng = thread_rng();
+       
+        if let Some(random_element) = dat.datum..choose(&mut rng) {
+            println!("Random element from Vec<u8>: {}", random_element);
+        } else {
+            println!("The vector is empty.");
+        }
+
+        let z = self.tel + (random_element*100);
+    }
+
+    pub fn receive(&self) -> i32 {
+        let y = self.rec + self.tel as i32;
+        return y;
+    }
 }
 
 
-// redirects main basically forever must be updated for main args..
-pub fn main_ad(){
+pub fn main_redirect(sli: slider) -> bool {
+    let mut rng = rand::thread_rng();
+    let len: i32 = rng.gen_range(-100..=100);
+    return help_wanted(len);
+}
+
+fn help_wanted (len: i32) -> bool {
+    let mut rng = rand::thread_rng();
+    let wrb: bool = rng.gen_bool(0.50);
+    return wrb
+}
+
+pub fn logic_redirect(oldcargo: cargo) -> cargo {
     let c = cargo {
         tel: 20,
         rec: 50,
     };
 
+    let y = oldcargo.send();
+    println!("{}",c.receive(););
+    
+    c.pipeclean();
+
     println!("{}",c.send());
 }
 
+struct slider {
 
-// now you want all data to essentially only be strings
+}
 
-struct data_pack {
+pub fn main_ad(){
+    let oldcargo = cargo {
+        tel: 10,
+        rec: 20,
+    };
+
+    let y = slider {};
+
+    while true {
+        let x = main_redirect(slider);
+        if x {
+            oldcargo = logic_redirect(oldcargo);
+        }    
+    }
 
 }
