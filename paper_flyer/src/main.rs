15d435bfd5dae9fdd87efa10a2663658b398bd42
GarciaNoel <n6garcia@ucsd.edu>
2025-09-26 13:03:18 -0700

    paper_flyer

--- paper_flyer/src/main.rs
+++ paper_flyer/src/main.rs
@@ -2,16 +2,22 @@
 
 use rand::Rng;
 
+#[derive(Copy, Clone)]
+struct slider;
+
 fn main() {
+    let y = slider;
+    let yy = y.clone();
+
     while true {
-        let x = main_redirect();
+        let x = main_redirect(yy);
         if x {
             logic_redirect();
         }    
     }
 }
 
-pub fn main_redirect() -> bool {
+pub fn main_redirect(sli: slider) -> bool {
     let mut rng = rand::thread_rng();
     let len: i32 = rng.gen_range(-100..=100);
     return help_wanted(len);
@@ -23,6 +29,7 @@ fn help_wanted (len: i32) -> bool {
     return wrb
 }
 
+//HERE PUT FOR LOOP LOGIC FOR ADDING W/ STATS [TODO]
 pub fn logic_redirect() {
     let x = 1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1;
 }
