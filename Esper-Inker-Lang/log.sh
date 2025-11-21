0cb7caf107ca5c7ea7381dc26f3b76c2dd107b98
GarciaNoel <n6garcia@ucsd.edu>
2025-11-20 16:28:19 -0800

    micro inker

---
+++ Esper-Inker-Lang/log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Unknown command:
+# Unknown command:
+echo "Looping..."
+echo "Looping..."
+# Unknown command:
+echo "Welcome to the ritual."
+# Unknown command: if mood == "ambient-chaos" then echo("Mood confirmed.")
+# Unknown command:
+echo "Loop complete." > ritual.log
+cat ritual.log
---
+++ Esper-Inker-Lang/microinker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true; do
+  ./log.sh   # replace with the path to your .sh file
+  sleep 0.333        # wait for 1/3 of a second
+done
