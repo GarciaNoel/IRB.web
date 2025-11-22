b2ba599c953d089dae675bc3f9b0dc1d67a3d0a7
GarciaNoel <n6garcia@ucsd.edu>
2025-11-21 20:02:46 -0800

    BitLC

---
+++ BitLC/Plist.js
@@ -0,0 +1,74 @@
+// Plist.js an THEORY OF CHOICES WRAPPER FOR .OLC FILES
+
+// maybe one lib to one liner shredder this file
+
+// in this javascript file we define the Plist class, which represents a property list (plist) file.
+
+class interpoterrover {
+    // left for interop purposes
+}
+
+// [TODO] FINISH DECLS OF WH3P AND WRAPPER CLASSES ALL ONE LINE FUNCS OK!
+// CORRECT STRUCTURE FOR Plist.js MOVE ON
+// 4 flags are first keys ok [DOTTEDLIST, MESSAGE, KEY, SECRET]
+// WHAT DOES IT DO
+class wh3p {
+    constructor(data = {}) {
+        this.data = data;
+    }
+
+    // all keys in '[]' and secrets in '/'
+    startsWith() {
+        
+    }
+
+    // all messages in '[[]]' broken by ';'
+    endsWith() {
+
+    }
+
+    // all words found by keys dots signature edges from secrets of all '../[]'
+    includes() {
+        
+    }       
+}
+
+// included file definitely needed to one line the wrapper class
+class wrapper {
+    constructor(data = {}) {
+        this.data = data;
+    }
+
+    // write OLC file edits from {}
+    write() {
+
+    }
+
+    // reads in OLC file format to {}
+    read() {
+
+    }
+}
+
+// APOINTISALISTASTERISKBASICALLYASETOFKEYVALUEPAIRS
+class Plist {
+    constructor(data = {}) {
+        this.data = data;
+    }
+
+    // Method to get a value by key
+    get(key) {
+        return this.data[key];
+    }
+
+    // Method to set a value by key
+    set(key, value) {
+        this.data[key] = value;
+    }
+
+    // Method to remove a key-value pair
+    remove(key) {
+        delete this.data[key];
+    }
+}
+// CORRECT STRUCTURE FOR Plist.js MOVE ON
