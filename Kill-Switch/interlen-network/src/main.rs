24804f5987020e52205cb73858a3d90b323a7610
GarciaNoel <n6garcia@ucsd.edu>
2025-12-17 12:55:00 -0800

    call fzbr componens like die not slime

--- Kill-Switch/interlen-network/src/main.rs
+++ Kill-Switch/interlen-network/src/main.rs
@@ -1,3 +1,228 @@
 fn main() {
-    println!("DODERA!");
+    println!("DODERA!
+HOOK
+
+589975558555555522211144775552
+
+CALL
+
+how to dial pad hm invention connect to fraddy fazber i see his scroutchy ah already connected to it
+
+
+DAS KOMPUTER
+killl resut oiinfen inoniongriubragiubreaiugojngdaowu
+    ienoinganorg    pplensnfeosingkndgineig
+
+    eignoirngounaorpungoupernetstetststekntingirneaoign[sgmk
+    a
+    kngks;g;
+    'd,g;s,g,dsgms;'kgksnakgnlkasnh;kdnkhnsalkn;lkhnd;lkahnnsog
+    
+    sagkrtnabint
+    [
+        aobrknb'kdmb;md;bm
+        bmtlmztl
+        t
+        fyjyj
+        muokokgplrvkmmvmo[eso[kp[lesksl,dslmvk'ndkvcl
+        
+        doderav,;s,lfs'gfojfoskeorst
+        
+        kmkgn;knofokfmrsmtmmf'ojrs'ogjodmhknghlkn;ofniojg;lesksldk
+        '
+        dkgsjgjih4piks[eog
+        [sgrs
+        lgk
+        [orsjgosrj;gjsigrog
+        sr[gk
+        [rsgojrspigirsjgjrspjgrsjofooesjfjigrnsou;hblkrsngs;gnursngs'old]sagkrtnabint
+        gfojfoskeorstsrgsj'rnslgnsononrnorm'lskgnrosunyl;krsmps
+        ]skg'kmsj'gisrigjisgrs
+        
+        rsigrsigirnkg
+        sl,'osmrpgmrv,]
+        ,g
+        
+        eignoirngounaorpungoupernetstetststekntingirneaoignjgrig'eiaa'lkr[
+            qk
+            nks';kgmrs;m'dkgsjgjih4piks
+            '
+            gldrlrgmiuy'dyjlh]tjd[gk,s
+            
+            ekmgisvisrjg
+            sokgskgowpgvospog,zr
+            dkh;dshknhk;kns,t.g.sgmkkrsh\rwj,;S;'
+            kh;kfd;hk;dmfh;dmlhf;
+            
+            'fldlshmfmdh;lmdfhs,'sdf,h
+            /,fds.f]
+        ]]]]]]
+    ]
+
+    egpkamiaini[oa
+    fazbermoejgnolfa
+    
+    femoamoejgnolfa
+    lmfalm  m in feoan gesmm omoiningounovna
+    g;lsem;ls;'mgds'kgndsa
+    gs'
+    mahanr;k
+    hn;k
+    n;knkrannrvounaogruio;nitsupyh'npuiy/ksyhdgtilsriguyrbgiaoyruea;hlyjtkdstyhsljt;gbqwyoupqdto
+    s[
+        SyuSuRSH
+
+        SyuSuRSH;hblkrsngshw
+        y;khgwa
+        hblkrsngs   
+        knknksgnsklngwuipogyidtuy[pjipnigaojhl;ghk
+        dialsa;
+        gldrlrgmiuy;rka
+        rak'lgjao
+        jgagaijiknskvnds;'
+        asd
+        ga'
+        wg
+        lvmomobmriborpslg
+        ,ofifna'vamsbromobmobrinfpem;'ew
+        fldlshmfmdhj'ml,
+        ',  
+            .'
+                ,'
+                    ,   '.'wgf, ,fazber 
+                        f
+                            .
+                            f,w
+                                ;fmw    
+                                f;mw
+                                    f;
+                                    wm;wmfm;wg  fm;wg
+                                    
+                                            
+                                        fmemapsomrspv
+                                            
+                                        
+                                            
+                                            fl[emlmb jfienv awjjeni'gnem'.fa,
+                                                
+                                                
+                                                .fwkmgn'g'l
+                                                ah  
+                                                    
+                                                    md;fekgnojgoa
+                                                        ,;wmaneag';ek;f
+                                                            
+                                                            
+                                                            
+                                                        
+                                                            \  [g;a<mvks kgns,
+                                                            
+                                                            
+                                                            
+                                        fmomgaf;wa''
+                                        la;hrmoa\;.
+                                    f\h;:fdh;'
+                                    muokokgplrvkmmvmo\]
+                                    \h;ah,
+                                    ;tk\l,Ma||F'bm[.eog
+                                    rh='tjds1'b;
+                                    zdrl]p.[\b/];\ra=p;\b]la\qw4,\hpmob]n]=KW]
+                                    [3GNOU[4\
+                                    F=RB'\1EWA\]
+                                    ;VQ\WE=;W4\A,RW
+                                    G
+                                    [L,EA
+                                    G/esG,EWL
+                                    'GMWAMGSEVMOWpmgo[mw;
+                                    ]hsarhperhwyohnqwpa
+                                    f'g]]]]]]]
+    ]]fpwgppw]e'g\]w;;1ds
+    sagkrtnabinth;'
+    sa
+    hmmhms;am;mdpsakgmiwnvodnsmpvanpduIAL'fSAGDJ;gsANG
+    'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE
+    'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE
+        
+        'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE
+            
+            
+        
+            
+            
+            
+            
+        
+            
+
+
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+
+
+
+
+
+
+
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+
+
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'   
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'           'LLAGDJDJNIGNPOAINAINOUAUUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;R
+SGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;R  SGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+
+
+
+
+
+
+
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+
+
+
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;R  
+SGNRNOAUGNORAOAOIVJP[WAKODJIPFH;;IFIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;R  
+
+
+SGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+            'LDSAGDJNIGNPOWAINOUAOGUOVNR;RANSGNRNOAUGNORAOIVJP[WAKODJNOIPFH;SOIFPJLE'
+
+            
+]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]*/
+]]]]]]]*/*/"
 }
