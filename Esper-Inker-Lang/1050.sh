c9f5ec665da05c09e76a61974b0106d89b855f5b
GarciaNoel <n6garcia@ucsd.edu>
2026-01-07 19:15:52 -0800

    temple os script

---
+++ Esper-Inker-Lang/1050.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#WINFORMSSLEPERAGENTORANGEPURPLE
+#x/z = 10 logins per second 50 accounts per 4 hours unsentient
+#$# while 1
+# Initialize a counter for the while loop
+count=1
+
+# Start the while loop
+while [ $count -le 10 ]; do
+  echo "--- While loop iteration: $count ---"
+
+  # $#for 10 graphics to 10 kanji 3 processes each
+  # First for loop
+  echo "  Starting first for loop..."
+  for i in {1..10}; do
+    #1 graphic SASPRINT per login ratio
+    python3 lll.py
+    #1 BINT per login ratio
+    cat ./my.txt
+    #1 SMUD kanjiet (temple_os)
+    cd ./TOS-Vertical-Overlay/
+    sudo timeout 10s ./runTOS
+    cd ../
+    #1 TEAR per login ratio
+    timeout 10s python3 lll.py
+    #1 kanjiet (run CLSMUDGE *classical*)
+    sudo timeout 10s python3 lll.py
+    #1 kanjiet (run AISMUDGE)
+    sudo python3 lll.py
+
+    echo "    First for loop iteration: $i"
+  done
+
+  # $# for 5 spam that run 10 login 50 account 4 hrs 2 PM
+  # HASHBLOCKBITLOCKER HBBL PROTOCOL SASPRINTER SNOT SLAM SLEDGE SXX NOTPROOFER
+  # Second for loop
+  echo "  Starting second for loop..."
+  for j in "apple" "banana" "cherry" "blue" "for"; do
+    # [TODO] ANTI SPAM BOTTER SPAM
+    cat ./my.txt
+    echo "    Second for loop item: $j"
+  done
+
+  # Increment the while loop counter
+  ((count++))
+done
+
+echo "Script finished."
+
+####binout///bin/bash
+
+
+
+
